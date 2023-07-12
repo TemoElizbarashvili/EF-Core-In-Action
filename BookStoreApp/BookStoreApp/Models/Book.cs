d42d4f0a330595b5bdbdf3cd3273de8a1e8b6411
TemoElizbarashvili <115801621+TemoElizbarashvili@users.noreply.github.com>
2023-07-12 20:40:14 +0400

    add bookstore app init

---
+++ BookStoreApp/BookStoreApp/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace BookStoreApp.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public DateTime PublishedOn { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+
+
+        //relationships
+
+
+    }
+}
